807439dcb0975cb8bc0cd0f48996c0515103e3f2
macibol775 <macibol775@student.polsl.pl>
2022-08-05 21:38:45 +0200

    expanded

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from base64 import encode
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('http://bocznica.eu/files/archiwum/aktualny.html')
+#downloading website
+html_text = requests.get('http://137.74.97.52/files/archiwum/aktualny.html')
 html_text.encoding = 'UTF-8'
 html_text = html_text.text
 soup = BeautifulSoup(html_text, 'lxml')
+#selecting trains
 train = soup.find_all('table', class_='table0')
 for t in train:
-    #print(t)
+    #dividing train into website parts
     cell = t.find_all('td')
+    #finding attributes
     attributeList = cell[1].text.replace(' ','').replace('\n\n','\n')
     attributeList = attributeList.rsplit("\n")
     attributeList.pop(0)
@@ -19,12 +22,23 @@ for t in train:
     attributes.pop(0)
     attributes.pop()
     infosRAW = cell[3]
-
-    print (attributeList)
+    attrList = {}
+    for a in range (len(attributeList)):
+        attrList[attributeList[a]] = attributes[a]
+    category = infosRAW.b.extract().text
+    name = infosRAW.b.extract().text
+    infosRAW = infosRAW.text.replace('  ','').replace('\n','').rsplit(' ')
+    code = infosRAW[1]
+    no = infosRAW[2]
+    #friendlyName for printing
+    friendlyName = category + ' ' + name + ' (' + no + ')'
+    print (attrList)
     print ('--')
-    print (attributes)
-    print ('--')
-    print (infosRAW)
+    print (friendlyName)
+    print ('----')
+    for a in range (4, len(cell)):
+        print(cell[a])
+        print ('--')
     print('-------------')
 
 
