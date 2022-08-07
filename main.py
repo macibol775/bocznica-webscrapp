542a255da42cc51e8670e2f46b672524be728937
macibol775 <macibol775@student.polsl.pl>
2022-08-07 13:01:52 +0200

    poprawki

--- main.py
+++ main.py
@@ -43,15 +43,42 @@ def isMultipleUnit(name):
          return 0
 
 def create_car(cell):
-    return 0
+    carNo = 0
+    carType = 0
+    infosA = cell.find_all('p', class_ = "klasaA")
+    infosAB = cell.find_all('p', class_ = "klasaAB")
+    infosB = cell.find_all('p', class_ = "klasaB")
+    if len(infosA) == 2:
+        carNo = infosA[0].text
+        carType = infosA[1].text
+    if len(infosAB) == 2:
+        carNo = infosAB[0].text
+        carType = infosAB[1].text
+    elif len(infosB) == 2:
+        carNo = infosB[0].text
+        carType = infosB[1].text
+
+    from_ = cell.find('p', class_ = "relOd")
+    if not (from_ == None):
+        from_ = from_.text.replace(' - ', '')
+    
+    to = cell.find('p', class_ = "relDo")
+    if not (to == None):
+        to = to.text.replace(' - ', '')
+    ann = cell.find('div', class_ = "wagUwagi")
+    if not (ann == None):
+        ann = ann.text.replace('\n', '').replace('  ', '').replace(';', '')
+        ann = ann.rsplit('-')
+        ann = list(filter(None, ann)) #delete empty
+    return Car(carNo, None, carType, from_, to, ann)
 
 def deleteDuplicates(list_):
     list_ = list(dict.fromkeys(list_))
     return list_
 class Car:
-    def __init__(self, no, class_, type, relFrom, relTo, annotations):
+    def __init__(self, no, attributes, type, relFrom, relTo, annotations):
         self.no = no
-        self.class_ = class_
+        self.attributes = attributes
         self.type = type
         self.relFrom = relFrom
         self.relTo = relTo
@@ -71,22 +98,27 @@ allCirculations = [] #DEBUG ONLY
 for t in train:
     #dividing train into website parts
     cell = t.find_all('td')
-    #finding attributes
+    #finding attribute names
     attributeList = cell[1].text.replace(' ','').replace('\n\n','\n')
     attributeList = attributeList.rsplit("\n")
     attributeList.pop(0)
     attributeList.pop()
+    #finding attribute values
     attributes = cell[2].text.replace('    ','').replace('\n\n','\n')
     attributes = attributes.rsplit("\n")
     attributes = list(filter(None, attributes))
-    infosRAW = cell[3]
+    #train no, name, code, category
+    infosRAW = cell[3]  
     attrList = {}
     for a in range (len(attributeList)):
         attrList[attributeList[a]] = attributes[a]
         if (attributeList[a] == 'Obiegi:'):
-            attrList[attributeList[a]] = re.split(' ; |, | - |/', attributes[a])
-            attrList[attributeList[a]] = deleteDuplicates(attrList[attributeList[a]])#delete duplicates
-            attrList[attributeList[a]] = list(filter(None, attrList[attributeList[a]])) #delete empty
+            attrList['Obiegi:'] = re.split(' ; |, | - |/', attributes[a])
+            for ob in range (len(attrList['Obiegi:'])):
+                if len(attrList['Obiegi:'][ob]) > 10:
+                    attrList['Obiegi:'][ob] = ''
+            attrList['Obiegi:'] = deleteDuplicates(attrList[attributeList[a]])#delete duplicates
+            attrList['Obiegi:'] = list(filter(None, attrList[attributeList[a]])) #delete empty
     category = infosRAW.b.extract().text    #IC/EIC/EIP/TLK
     name = infosRAW.b.extract().text
     infosRAW = infosRAW.text.replace('  ','').replace('\n','').rsplit(' ')
@@ -100,8 +132,8 @@ for t in train:
     print ('----')
     locos = [] #array of locomotives
     cars = []  #array of cars
-    for a in range (4, len(cell)):
-        cars_count = 0
+    cars_count = 0
+    for a in range (4, len(cell) - 1):
         if not (cell[a].get('class')):  #ommit loco separator
             lok = cell[a].find('p', class_ = "Lok")
             if (lok):   #if it's a loco
@@ -112,26 +144,30 @@ for t in train:
                         locos[len(locos) - 1].relTo = cell.find('p', class_ = "relDo").text.replace(' - ', '')
                     else:   #loco is different
                         locos.append(create_loco(cell[a]))
-                for l in locos:
-                    #print (l)
-                    allLocos.append(l.name)
+
             elif (isMultipleUnit(locos[0].name) == 0):
                 cars.append(create_car(cell[a]))
             else:
                 cars_count += 1
     if isMultipleUnit(locos[0].name):
-        times = isMultipleUnit(locos[0].name) / cars_count
+        times = cars_count / isMultipleUnit(locos[0].name)
         if not times == 1:
             locos[0].name = str(times) + 'x ' + str(locos[0].name)
+    for l in locos:
+        print (l)
+        allLocos.append(l.name)
+    for c in cars:
+        print (str(c.no))
     print('-------------')
     for p in attrList['Obiegi:']:
         allCirculations.append(p)
 print ('================================================================')
+print ('Obiegi:')
 allCirculations = deleteDuplicates(allCirculations)
 allLocos = deleteDuplicates(allLocos)
 for c in allCirculations:
     print (c)
-print ('================================================================')
+print ('Lokomotywy i EZT:')
 for l in allLocos:
     print (l)
 
